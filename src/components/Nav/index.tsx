e9f19b54bcd4e964417df660a3987a776be2af3b
niles87 <nbing19@gmail.com>
2020-05-24 16:35:43 -0700

    added style to nav component and flex layout for entire app

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
-const Nav = () => {
-  return <div>Nav</div>;
+const Nav = (): JSX.Element => {
+  return (
+    <Fragment>
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <h3>Niles </h3>
+          </Link>
+        </div>
+        <div className="links">
+          <div>
+            <Link to="/projects">Projects</Link>
+          </div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          {/* <div >
+              <Link to="/about">About</Link>
+            </div> */}
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Nav;
