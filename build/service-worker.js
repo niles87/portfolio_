bba5d6b38db8fc660f5af53a60116bac03c68503
niles87 <nbing19@gmail.com>
2020-05-27 20:05:40 -0700

    updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d20f9610cafb97d211bd559a254d8402.js"
+  "/precache-manifest.e4785fe7a87f01b0609ac98b241b3ee4.js"
 );
 
 self.addEventListener('message', (event) => {
